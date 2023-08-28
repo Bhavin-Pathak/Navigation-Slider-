4c8d0ad5ff550cfefcf11283557a9eb663c7d3a7
rvshah2217 <rvshah2217@gmail.com>
2023-08-28 17:56:41 +0530

    Initial Commit

---
+++ Slider/SliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  SliderApp.swift
+//  Slider
+//
+//  Created by Ravi Shah on 28/08/23.
+//
+
+import SwiftUI
+
+@main
+struct SliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
